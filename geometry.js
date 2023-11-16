6cdfad37db2b33f85d137d97aac463bc91667d34
younuskhan1 <md.younuskhan412@gmail.com>
2023-11-16 19:24:14 +0600

    validation is done

--- geometry.js
+++ geometry.js
@@ -13,31 +13,39 @@ for (let btn of buttons) {
         const secondInputValue = event.target.parentNode.parentNode.childNodes[9].childNodes[1].childNodes[4].value;
         const secondInputValueNumber = parseFloat(secondInputValue);
 
-        const li = document.createElement("li");
-        li.classList.add("calculated-number");
-        const count = calculatedArea.childElementCount;
-
-        function setArea(name, area) {
-            li.innerHTML = `<p>${count + 1}. ${name} ${area} cm<sup>2</sup></p> 
-            <button type="button" class="convert-to-meter-button">meter<sup>2</sup></button>`;
-            calculatedArea.appendChild(li);
-        }
-
-        if (cardName === "Triangle" || cardName === "Rhombus" || cardName === "Pentagon") {
-            const areaOfIndicatedShape = .5 * firstInputValueNumber * secondInputValueNumber;
-            const areaOfIndicatedShapeDecimal = areaOfIndicatedShape.toFixed(2);
-            setArea(cardName, areaOfIndicatedShapeDecimal);
-
-        } else if (cardName === "Ellipse") {
-            const areaOfIndicatedShape = 3.1416 * firstInputValueNumber * secondInputValueNumber;
-            const areaOfIndicatedShapeDecimal = areaOfIndicatedShape.toFixed(2);
-            setArea(cardName, areaOfIndicatedShapeDecimal);
+        if (firstInputValueNumber < 0 || secondInputValueNumber < 0) {
+            alert("Please provide me a positive numeric value.")
+        } else if (isNaN(firstInputValueNumber) || isNaN(secondInputValueNumber)) {
+            alert("Do not give me text. Please provide me a positive numeric value.")
         } else {
-            const areaOfIndicatedShape = firstInputValueNumber * secondInputValueNumber;
-            const areaOfIndicatedShapeDecimal = areaOfIndicatedShape.toFixed(2);
-            setArea(cardName, areaOfIndicatedShapeDecimal);
+            const li = document.createElement("li");
+            li.classList.add("calculated-number");
+            const count = calculatedArea.childElementCount;
+
+            function setArea(name, area) {
+                li.innerHTML = `<p>${count + 1}. ${name} ${area} cm<sup>2</sup></p> 
+                <button type="button" class="convert-to-meter-button">meter<sup>2</sup></button>`;
+                calculatedArea.appendChild(li);
+            }
+
+            if (cardName === "Triangle" || cardName === "Rhombus" || cardName === "Pentagon") {
+                const areaOfIndicatedShape = .5 * firstInputValueNumber * secondInputValueNumber;
+                const areaOfIndicatedShapeDecimal = areaOfIndicatedShape.toFixed(2);
+                setArea(cardName, areaOfIndicatedShapeDecimal);
+
+            } else if (cardName === "Ellipse") {
+                const areaOfIndicatedShape = 3.1416 * firstInputValueNumber * secondInputValueNumber;
+                const areaOfIndicatedShapeDecimal = areaOfIndicatedShape.toFixed(2);
+                setArea(cardName, areaOfIndicatedShapeDecimal);
+            } else {
+                const areaOfIndicatedShape = firstInputValueNumber * secondInputValueNumber;
+                const areaOfIndicatedShapeDecimal = areaOfIndicatedShape.toFixed(2);
+                setArea(cardName, areaOfIndicatedShapeDecimal);
+
+            }
 
         }
+
     })
 }
 
